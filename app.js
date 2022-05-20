a91e99532ffb25b5c8ffeba3cfc7043b714b854a
meta-ela <anxhelameta@live.it>
2022-05-20 18:11:03 +0200

    Adds color variables

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ Dato un container nel DOM, appendi un elemento html con il numero o la stringa c
 MILESTONE 3
 Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5
 e per i valori che sono sia multipli di 3 che di 5.
+
+
+
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Come faccio a sapere se un numero è divisibile per un altro? 
 Abbiamo visto qualcosa di particolare che possiamo usare? 
@@ -32,6 +35,12 @@ Buon lavoro e buon divertimento!
 - dentro al ciclo creare if % 5 con text buzz
 - dentro al ciclo creare if con sia multiplo 3 che 5 text FizzBuzz
 - controllare con console.log la stampa
+- creare variabile che richiama la row d html
+- chiamare variabile all'interno del ciclo e sovrascrivvere html per stampare il console.log
+- stampare in inner.html anche tag element con classi bootstrap
+- creare classi per i 4 colori di sfondo dei riquadri
+- dichiararli come variabili in js
+- dare ad ogni if la variabile colore
 */
 
 
@@ -40,11 +49,17 @@ let resultListHtml = document.querySelector(".resultList");
 
 let text = "";
 
+let colorBlue = document.querySelector(".color_blue");
+let colorGreen = document.querySelector(".color_green");
+let colorYellow = document.querySelector(".color_yellow");
+let colorPink = document.querySelector(".color_pink");
+
 for (let i = 1; i <= 100; i++) {
     console.log();
 
     if (i % 3 === 0 && i % 5 === 0) {
         text = "FizzBuzz";
+        resultListHtml.classList.add("color_pink");
     } 
 
     else if (i % 3 === 0) {
@@ -62,6 +77,10 @@ for (let i = 1; i <= 100; i++) {
 
     console.log(text);
 
-    resultListHtml.innerHTML += `<div class="col">${text}</div>`;
+    resultListHtml.innerHTML += `<div class="col">
+                                <div class="py-5 px-5 text-center">
+                                ${text}
+                                </div>
+                                </div>`;
 
 }
